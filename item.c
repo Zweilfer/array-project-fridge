47192bea2ad22648fc877807d7c540562dcff57d
Garum <teeteech375@gmail.com>
2026-08-30 15:56:56 +0700

    Update item.c

--- item.c
+++ item.c
@@ -10,7 +10,14 @@ void create() {
 }
 
 void display() {
-  
+  if(count == 0){
+    printf("The fridge is empty.\n");
+    return 0;  
+  }
+  print("\n===== Fridge items =====\n");
+  for (int i =0; i< count; i++){
+    printf("%d. %s | %s | expire %s | qty %d\n", i + 1, fridge[i].name, fridge[i].type, fridge[i].expire, fridge[i].qty);
+  }
 }
 
 void search() {
@@ -34,10 +41,23 @@ void checkExpire() {
 }
 
 void saveFile() {
-  
+  FILE *fp = fopen("fridge.txt", "w");
+  if(fp == NULL) {
+    print("Cannot save file.\n");
+    return;
+  }
+
+  for (int i = 0; i < count; i++) {
+    fprintf(fp, "%s,%s,%s,%d\n", fridge[i].name, fridge[i].type, fridge[i].expire, fridge[i].qty);
+  }
+  fclose(fp);
+  print("Fridge data saved.\n");
 }
 
 void saveHistory(const char *detail){
+  FILE *fp = fopen("history.txt", "a");
+  if (fp == NULL) return;
 
-  
+  fprint(fp, "===== Picked items =====\n%s\n", detail);
+  fclose(fp);
 }
