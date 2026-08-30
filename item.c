614eaa28f8726ca8e0784cf1c7cefd1b5155f1c2
Garum <teeteech375@gmail.com>
2026-08-30 15:15:04 +0700

    Create item.c

---
+++ item.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "item.h"
+
+Item fridge[MAX];
+int count = 0;
+
+void create() {
+  
+}
+
+void display() {
+  
+}
+
+void search() {
+  
+}
+
+void insert() {
+  
+}
+
+void deleteItem() {
+  
+}
+
+void pickItems() {
+  
+}
+
+void checkExpire() {
+  
+}
+
+void saveFile() {
+  
+}
+
+void saveHistory(const char *detail){
+
+  
+}
