8efbfcb4865019c951648228632929989251c5a5
Garum <teeteech375@gmail.com>
2026-08-30 15:07:07 +0700

    Update main.c

--- main.c
+++ main.c
@@ -6,13 +6,14 @@ int main() {
   create();
 
   do{
-    printf("\n==== Fridge Inventory System =====\n");
+    printf("\n===== Fridge Inventory System =====\n");
     printf("1. Display all items in fridge\n");
     printf("2. Search\n");
     printf("3. Insert item\n");
     printf("4. Delete item\n");
     printf("5. Pick items\n");
     printf("6. Check expired items\n");
+    printf("7. Save file\n")
     printf("0. Exit");
     printf("Select menu: ")
     scanf("%d", &menu);
@@ -24,6 +25,7 @@ int main() {
       case 4: deleteItem(); break;
       case 5: pickItem(); break;
       case 6: checkExpire(); break;
+      case 7: saveFile(); break;
       case 0: printf("Goodbye.\n"); break;
       default: printf("Invalid menu.\n");
     } while (menu != 0);
